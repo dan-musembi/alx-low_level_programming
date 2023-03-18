8db11c4e6ebf35a3ca098f0518fcf16ef8c5de35
dan-musembi <danielmusembi427@gmail.com>
2023-03-18 04:01:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
+#define fp fputs
 /**
  * main - A c program that prints a line to the standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fp("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
